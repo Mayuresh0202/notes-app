6bb04168c130da2fa190a6a92329f697fbe420bc
Mayuresh Kadam <kadammayuresh721@gmail.com>
2023-07-30 15:39:29 +0530

    Add TODO Features like Adding a Todo, Removing a Todo, Saving the todo in Localstorage to persist between app restarts & Toggle Theme

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,useEffect } from 'react'
+import TodosList from './components/TodosList'
+import {nanoid} from 'nanoid'
+import Search from './components/Search';
+import Header from './components/Header';
 
 function App() {
+{/*here we are adding three dummy Todos in our useState hooks for Todos*/}
+  const [notes,setNotes] = useState([
+]);
+{/*this are the staes for searching text and toggle mode*/}
+
+const [searchNote,setSearchNote] = useState('');
+const [darkMode,setDarkMode] = useState(false)
+
+{/*to store or save the data in local storage even after closing the tab*/}
+useEffect(()=>{
+  const savedNotes = JSON.parse(localStorage.getItem('notes-app-data'))
+  if(savedNotes){
+    setNotes(savedNotes)
+
+  }
+},[])
+{/*this is for saving the app data in local storage*/}
+useEffect(()=>{
+  localStorage.setItem('notes-app-data',JSON.stringify(notes))
+},[notes])
+{/*this function is for adding new note*/}
+const addTodo = (text) =>{
+  
+  const date = new Date();
+  const newTodo = {
+    id:nanoid(),
+    text : text,
+    date : date.toLocaleDateString()
+  }
+  const newNotes = [...notes,newTodo]
+  setNotes(newNotes)
+  newNotes.reverse();
+}
+const deletingAllTodos = () =>{
+  setNotes([]);
+}
+{/*this function is for deleting notes in note app with a id*/}
+const deletingTodo = (id) =>{
+ const newNotes = notes.filter((note)=> note.id!==id);
+// const nt =  notes.filter((note)=> note.id===id)
+ setNotes(newNotes)
+
+
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+    <div className={`${darkMode && 'dark-mode'}`}>
+          <div className="container">
+      <Header handleDeleteAllTodos={deletingAllTodos} handleToggleDarkMode={setDarkMode}/>
+
+      <Search handleSearch={setSearchNote}/>
+      
+      <TodosList notes={notes.filter((note)=>note.text.toLowerCase().includes(searchNote))} handleAddTodo={addTodo} handleDelete={deletingTodo}/>
+
     </div>
-  );
+    </div>
+
+  )
 }
 
-export default App;
+export default App
